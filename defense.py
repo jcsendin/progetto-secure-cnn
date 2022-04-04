e777d5f7191b16103a2bca9b144049955c89304e
Cristobal <jcsenmar@gmail.com>
2022-04-04 19:15:55 +0200

    Added two defensive methods against FGSM

---
+++ defense.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+#created by jcsendin on the 3/April/2022
+
+
+#INFORMATION-------------------------------
+#In this file we can find two defensive approaches against a FGSM attack
+
+
+#IMPORTS-----------------------------------
+import tensorflow as tf
+import numpy as np
+
+from fgsm import create_adversarial_image
+from sklearn.utils import shuffle
+
+
+#FUNCTIONS---------------------------------
+
+def generate_adversarial_batch(model, total, images, labels, dims, eps=0.1):
+	while True:
+		perturbImages = []
+		perturbLabels = []
+
+		idxs = np.random.choice(range(0, len(images)), size = total, replace = False)
+
+		for i in idxs:
+			image = images[i]
+			image = tf.convert_to_tensor(image)
+			image = image[None, ...]
+			label = labels[i]
+
+			adversary = create_adversarial_image(model, image, label, eps)
+
+			perturbImages.append(tf.reshape(adversary, dims))
+			perturbLabels.append(label)
+
+		yield (np.array(perturbImages), np.array(perturbLabels))
+
+
+def generate_mixed_adversarial_batch(model, total, images, labels, dims, eps=0.01, split=0.5):
+	#the split indicates the percentage of adversarial images
+	totalAdv = int(total * split)
+	totalNormal = int(total * (1 - split))
+
+	while True:
+		idxs = np.random.choice(range(0, len(images)),
+			size=totalNormal, replace=False)
+		mixedImages = images[idxs]
+		mixedLabels = labels[idxs]
+		
+		idxs = np.random.choice(range(0, len(images)), size=totalAdv,
+			replace=False)
+  
+		for i in idxs:
+			image = images[i]
+			image = tf.convert_to_tensor(image)
+			image = image[None, ...]
+			label = labels[i]
+
+			adversary = create_adversarial_image(model, image, label, eps)
+
+			mixedImages = np.vstack([mixedImages, adversary])
+			mixedLabels = np.vstack([mixedLabels, label])
+
+		(mixedImages, mixedLabels) = shuffle(mixedImages, mixedLabels)
+
+		yield (mixedImages, mixedLabels)
